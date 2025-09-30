df4afc885b9a93e959c6889c78ac345d7a171660
cforChobar <sidhuashman02@gmail.com>
2025-09-30 09:52:20 +0530

    frame placement on the wall with my image in the center

--- src/components/RoomContent.tsx
+++ src/components/RoomContent.tsx
@@ -3,6 +3,8 @@ import Desk from "./Desk"
 import Computer from "./Computer"
 import { useControls } from "leva"
 import Bookshelf from "./Bookshelf"
+import Frame from "./Frame"
+import { Image } from "@react-three/drei"
 // import Chair from "./Chair"
 
 const RoomContent = () => {
@@ -12,8 +14,22 @@ const RoomContent = () => {
 			value: {
 				x: -2.204,
 				y: 0.051,
-				z: -1.383,
+				z: -1.36,
 			},
+			step: 0.001,
+		},
+	})
+	// position of the frame group
+	const { framePosition } = useControls("frames", {
+		framePosition: {
+			value: { x: 0, y: 2.15, z: -1.98 },
+			step: 0.001,
+		},
+	})
+	// scale of our image
+	const { imageScale } = useControls("Image", {
+		imageScale: {
+			value: { x: 0.63, y: 0.63 },
 			step: 0.001,
 		},
 	})
@@ -36,6 +52,20 @@ const RoomContent = () => {
 
 			{/* Bookshelf on back wall along with the door */}
 			<Bookshelf position={[-2.02, 1.5, 1.84]} />
+
+			{/* Frames on the front wall */}
+			<group
+				position={[framePosition.x, framePosition.y, framePosition.z]}
+			>
+				<Frame scale={0.5} position-x={0.6} />
+				<Frame scale={1} />
+				<Image
+					url="/images/my-image.jpg"
+					scale={[imageScale.x, imageScale.y]}
+					position-z={0.01}
+				/>
+				<Frame scale={0.5} position-x={-0.6} />
+			</group>
 		</>
 	)
 }
