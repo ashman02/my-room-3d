fbfc604c6777ac2f486d731c07bf60d9ffd89f62
cforChobar <sidhuashman02@gmail.com>
2025-10-02 10:08:33 +0530

    added single light in the room as of now used point light to mimic bulb

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 					fov: 45,
 					near: 0.1,
 					far: 200,
-					position: [0, 2, 6],
+					position: [0, 2, 0],
 				}}
 			>
 				<Perf position="top-left" />
--- src/components/Lights.tsx
+++ src/components/Lights.tsx
@@ -1,10 +1,35 @@
-
+// import { useHelper } from "@react-three/drei"
+import { useControls } from "leva"
+import { useRef } from "react"
+import * as THREE from "three"
 
 const Lights = () => {
+	const pointLightRef = useRef<THREE.Object3D>(null)
+	// useHelper(pointLightRef, THREE.PointLightHelper)
+
+
+	// position of the bulb
+	const { pointPosition, intensity, decay, color } = useControls("Point Light", {
+		pointPosition: {
+			value: { x: 0.02, y: 2.64, z: 1.56 },
+			step: 0.01,
+		},
+		intensity : { value: 4.0, min: 0, max: 10, step: 0.1 },
+		decay : { value: 0.6, min: 0, max: 10, step: 0.1 },
+		color : {value : "#a4d6e8"},
+	})
+
 	return (
 		<>
-			<ambientLight intensity={0.5} />
-			<directionalLight position={[1, 2, 3]} intensity={1.5} />
+			{/* Ambient light acting like moon light */}
+			<ambientLight intensity={0.2} color={"#ffffff"} />
+
+			{/* We are going to use point light to mimic light bulb */}
+			<pointLight
+				ref={pointLightRef}
+				args={[color, intensity, 0, decay]}
+				position={[pointPosition.x, pointPosition.y, pointPosition.z]}
+			/>
 		</>
 	)
 }
