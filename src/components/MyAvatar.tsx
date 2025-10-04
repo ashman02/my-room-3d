d11bfdfed1592416e3a66b6d3ea2246b219c7889
cforChobar <sidhuashman02@gmail.com>
2025-10-04 07:56:09 +0530

    added physics to the avatar

--- src/components/MyAvatar.tsx
+++ src/components/MyAvatar.tsx
@@ -1,9 +1,14 @@
+import { CuboidCollider, RigidBody } from "@react-three/rapier"
 import Avatar from "./models/Avatar"
 
 const MyAvatar = () => {
 	return (
 		<>
-			<Avatar scale={0.8} position={[0.8, 0.06, 1]} />
+			<RigidBody colliders={false} scale={0.8} position={[0.8, 0.06, 1]}>
+				<Avatar />
+				<CuboidCollider args={[0.4, 0.8, 0.15]} position={[0, 0.8, 0]} />
+				<CuboidCollider args={[0.1, 0.12, 0.1]} position={[0, 1.73, 0.03]} />
+			</RigidBody>
 		</>
 	)
 }
