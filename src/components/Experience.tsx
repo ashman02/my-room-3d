9bca6a0911f126baf84b489c1b63fcb053cddd4d
cforChobar <sidhuashman02@gmail.com>
2025-10-08 07:45:44 +0530

    finally camera is following our character identified the bug as well

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,4 +1,3 @@
-import { OrbitControls } from "@react-three/drei"
 import Room from "./Room"
 import RoomContent from "./RoomContent"
 import Lights from "./Lights"
@@ -9,7 +8,6 @@ import { Physics } from "@react-three/rapier"
 const Experience = () => {
 	return (
 		<>
-			<OrbitControls makeDefault />
 			<color args={["#041A40"]} attach={"background"} />
 			<Lights />
 			<Physics debug>
--- src/components/MyAvatar.tsx
+++ src/components/MyAvatar.tsx
@@ -1,13 +1,49 @@
-import { CuboidCollider, RigidBody } from "@react-three/rapier"
+import { RapierRigidBody, RigidBody } from "@react-three/rapier"
 import Avatar from "./models/Avatar"
+import { useRef } from "react"
+import { Group, Vector3 } from "three"
+import { useFrame } from "@react-three/fiber"
+
 
 const MyAvatar = () => {
+	const rigidBodyRef = useRef<RapierRigidBody>(null)
+	const container = useRef<Group>(null)
+	const character = useRef<Group>(null)
+	const cameraTarget = useRef<Group>(null)
+	const cameraPosition = useRef<Group>(null)
+	const cameraWorldPosition = useRef(new Vector3())
+	const cameraLookAtWorldPosition = useRef(new Vector3())
+	const cameraLookAt = useRef(new Vector3())
+
+	useFrame(({ camera }) => {
+		if (!cameraPosition.current) return
+
+		cameraPosition.current.getWorldPosition(cameraWorldPosition.current)
+		camera.position.lerp(cameraWorldPosition.current, 0.1)
+
+		if (cameraTarget.current) {
+			cameraTarget.current.getWorldPosition(
+				cameraLookAtWorldPosition.current
+			)
+			cameraLookAt.current.lerp(cameraLookAtWorldPosition.current, 0.1)
+
+			camera.lookAt(cameraLookAt.current)
+		}
+	})
 	return (
 		<>
-			<RigidBody colliders={false} scale={0.8} position={[0.8, 0.06, 1]}>
-				<Avatar />
-				<CuboidCollider args={[0.4, 0.8, 0.15]} position={[0, 0.8, 0]} />
-				<CuboidCollider args={[0.1, 0.12, 0.1]} position={[0, 1.73, 0.03]} />
+			<RigidBody ref={rigidBodyRef} lockRotations>
+				<group ref={container}>
+					<group ref={cameraTarget} position-z={4} />
+					<group
+						ref={cameraPosition}
+						position-y={1.8}
+						position-z={-1}
+					/>
+					<group ref={character}>
+						<Avatar scale={0.8} />
+					</group>
+				</group>
 			</RigidBody>
 		</>
 	)
